82707186cdf444778f8ae3c6edfd077e9f8a28bc
Hem Jora <107682061+Jorahem@users.noreply.github.com>
2024-05-25 09:56:58 +0000

    string

---
+++ string.py
@@ -0,0 +1,100 @@
+#  Single line comment
+letter = 'P'                # A string could be a single character or a bunch of texts
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  # String could be  a single or double quote,"Hello, World!"
+print(greeting)             # Hello, World!
+print(len(greeting))        # 13
+sentence = "I hope you are enjoying 30 days of python challenge"
+print(sentence)
+
+# Multiline String
+multiline_string = '''I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python.'''
+print(multiline_string)
+# Another way of doing the same thing
+multiline_string = """I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python."""
+print(multiline_string)
+
+# String Concatenation
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+space = ' '
+full_name = first_name  +  space + last_name
+print(full_name) # Asabeneh Yetayeh
+# Checking length of a string using len() builtin function
+print(len(first_name))  # 8
+print(len(last_name))   # 7
+print(len(first_name) > len(last_name)) # True
+print(len(full_name)) # 15
+
+#### Unpacking characters 
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t 
+print(d) # h
+print(e) # o
+print(f) # n
+challenge = 'thirty days of python'
+print(challenge.islower()) # True
+challenge = 'Thirty days of python'
+print(challenge.islower()) # False
+
+# isupper(): returns if all characters are uppercase characters
+
+challenge = 'thirty days of python'
+print(challenge.isupper()) #  False
+challenge = 'THIRTY DAYS OF PYTHON'
+print(challenge.isupper()) # True
+
+
+# isnumeric():Checks numeric characters
+
+num = '10'
+print(num.isnumeric())      # True
+print('ten'.isnumeric())    # False
+
+# join(): Returns a concatenated string
+
+web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
+result = '#, '.join(web_tech)
+print(result) # 'HTML# CSS# JavaScript# React'
+
+# strip(): Removes both leading and trailing characters
+
+challenge = ' thirty days of python '
+print(challenge.strip('y')) # 5
+
+# replace(): Replaces substring inside
+
+challenge = 'thirty days of python'
+print(challenge.replace('python', 'coding')) # 'thirty days of coding'
+
+# split():Splits String from Left
+
+challenge = 'thirty days of python'
+print(challenge.split()) # ['thirty', 'days', 'of', 'python']
+
+# title(): Returns a Title Cased String
+
+challenge = 'thirty days of python'
+print(challenge.title()) # Thirty Days Of Python
+
+# swapcase(): Checks if String Starts with the Specified String
+  
+challenge = 'thirty days of python'
+print(challenge.swapcase())   # THIRTY DAYS OF PYTHON
+challenge = 'Thirty Days Of Python'
+print(challenge.swapcase())  # tHIRTY dAYS oF pYTHON
+
+# startswith(): Checks if String Starts with the Specified String
+
+challenge = 'thirty days of python'
+print(challenge.startswith('thirty')) # True
+challenge = '30 days of python'
+print(challenge.startswith('thirty')) # False
