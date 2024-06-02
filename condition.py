a8ec2df21f86d084aa6e16815d72bb1337ec266b
Hem Jora <107682061+Jorahem@users.noreply.github.com>
2024-06-02 08:24:39 +0000

    condition

---
+++ condition.py
@@ -0,0 +1,13 @@
+a = 3
+if a < 0:
+    print('A is a negative number')
+else:
+    print('A is a positive number')
+
+
+user = 'James'
+access_level = 3
+if user == 'admin' or access_level >= 4:
+        print('Access granted!')
+else:
+    print('Access denied!')
